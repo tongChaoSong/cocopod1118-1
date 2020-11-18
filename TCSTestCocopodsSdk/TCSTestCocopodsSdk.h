6df3749ff157b3079bcf593f7122c0518ba48216
青春丶不过如此 <447154692@qq.com>
2020-11-18 15:28:44 +0800

    1

---
+++ TCSTestCocopodsSdk/TCSTestCocopodsSdk.h
@@ -0,0 +1,19 @@
+//
+//  TCSTestCocopodsSdk.h
+//  TCSTestCocopodsSdk
+//
+//  Created by Apple on 2020/11/18.
+//  Copyright © 2020 tcs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TCSTestCocopodsSdk.
+FOUNDATION_EXPORT double TCSTestCocopodsSdkVersionNumber;
+
+//! Project version string for TCSTestCocopodsSdk.
+FOUNDATION_EXPORT const unsigned char TCSTestCocopodsSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCSTestCocopodsSdk/PublicHeader.h>
+
+
